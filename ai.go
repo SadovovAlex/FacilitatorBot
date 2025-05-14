6c17c42741cb6b99ba4070d539e794fe88167ca2
SadovovAlex <aasdvv@gmail.com>
2025-05-14 22:51:43 +0300

    add ai req func

--- ai.go
+++ ai.go
@@ -4,20 +4,45 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-// generateSummary создает краткую сводку с помощью локальной LLM
-func (b *Bot) generateSummary(messages string) (string, error) {
-	prompt := fmt.Sprintf(b.config.SummaryPrompt, messages)
+func (b *Bot) generateAiRequest(systemPrompt string, prompt string, chatID int64) (string, error) {
+	// Отправляем индикатор печати
+	if _, err := b.tgBot.Request(tgbotapi.NewChatAction(chatID, tgbotapi.ChatTyping)); err != nil {
+		log.Printf("Ошибка отправки индикатора печати: %v", err)
+	}
+	// Запускаем горутину для периодической отправки индикатора печати, канал stopTyping не забываем закрыть!!!
+	stopTyping := make(chan struct{})
+	go func() {
+		ticker := time.NewTicker(5 * time.Second) // Отправляем каждые 5 секунд
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				// Отправляем индикатор печати
+				chatAction := tgbotapi.NewChatAction(chatID, tgbotapi.ChatTyping)
+				if _, err := b.tgBot.Request(chatAction); err != nil {
+					log.Printf("Ошибка отправки индикатора печати: %v", err)
+				}
+			case <-stopTyping:
+				return
+			}
+		}
+	}()
+	defer close(stopTyping)
 
 	request := LocalLLMRequest{
 		Model: b.config.AiModelName, // Имя модели может быть любым для локальной LLM
 		Messages: []LocalLLMMessage{
 			{
 				Role:    "system",
-				Content: b.config.SystemPrompt,
+				Content: systemPrompt,
 			},
 			{
 				Role:    "user",
@@ -63,99 +88,196 @@ func (b *Bot) generateSummary(messages string) (string, error) {
 }
 
 // generateSummary создает краткую сводку с помощью локальной LLM
-func (b *Bot) generateAnekdot(messages string) (string, error) {
-	prompt := fmt.Sprintf(b.config.AnekdotPrompt, messages)
+// func (b *Bot) generateSummary(messages string, chatID int64) (string, error) {
+// 	// Отправляем индикатор печати
+// 	if _, err := b.tgBot.Request(tgbotapi.NewChatAction(chatID, tgbotapi.ChatTyping)); err != nil {
+// 		log.Printf("Ошибка отправки индикатора печати: %v", err)
+// 	}
+// 	// Запускаем горутину для периодической отправки индикатора печати, канал stopTyping не забываем закрыть!!!
+// 	stopTyping := make(chan struct{})
+// 	go func() {
+// 		ticker := time.NewTicker(5 * time.Second) // Отправляем каждые 5 секунд
+// 		defer ticker.Stop()
+// 		for {
+// 			select {
+// 			case <-ticker.C:
+// 				// Отправляем индикатор печати
+// 				chatAction := tgbotapi.NewChatAction(chatID, tgbotapi.ChatTyping)
+// 				if _, err := b.tgBot.Request(chatAction); err != nil {
+// 					log.Printf("Ошибка отправки индикатора печати: %v", err)
+// 				}
+// 			case <-stopTyping:
+// 				return
+// 			}
+// 		}
+// 	}()
+// 	defer close(stopTyping)
 
-	request := LocalLLMRequest{
-		Model: b.config.AiModelName, // Имя модели может быть любым для локальной LLM
-		Messages: []LocalLLMMessage{
-			// {
-			// 	Role:    "system",
-			// 	Content: b.config.SystemPrompt,
-			// },
-			{
-				Role:    "user",
-				Content: prompt,
-			},
-		},
-		Temperature: 0.4,
-		MaxTokens:   1000,
-	}
+// 	prompt := fmt.Sprintf(b.config.SummaryPrompt, messages)
 
-	jsonData, err := json.Marshal(request)
-	if err != nil {
-		return "", fmt.Errorf("ошибка маршалинга запроса: %v", err)
-	}
+// 	request := LocalLLMRequest{
+// 		Model: b.config.AiModelName, // Имя модели может быть любым для локальной LLM
+// 		Messages: []LocalLLMMessage{
+// 			{
+// 				Role:    "system",
+// 				Content: b.config.SystemPrompt,
+// 			},
+// 			{
+// 				Role:    "user",
+// 				Content: prompt,
+// 			},
+// 		},
+// 		Temperature: 0.6,
+// 		MaxTokens:   16000,
+// 	}
 
-	fmt.Println("Get AI request...")
-	resp, err := b.httpClient.Post(b.config.LocalLLMUrl, "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return "", fmt.Errorf("ошибка HTTP запроса: %v", err)
-	}
-	defer resp.Body.Close()
+// 	jsonData, err := json.Marshal(request)
+// 	if err != nil {
+// 		return "", fmt.Errorf("ошибка маршалинга запроса: %v", err)
+// 	}
 
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("неверный статус код: %d", resp.StatusCode)
-	}
+// 	fmt.Println("Get AI request...")
+// 	time.Sleep(15 * time.Second)
 
-	var response LocalLLMResponse
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return "", fmt.Errorf("ошибка декодирования ответа: %v", err)
-	}
+// 	resp, err := b.httpClient.Post(b.config.LocalLLMUrl, "application/json", bytes.NewBuffer(jsonData))
+// 	if err != nil {
+// 		return "", fmt.Errorf("ошибка HTTP запроса: %v", err)
+// 	}
+// 	defer resp.Body.Close()
 
-	if len(response.Choices) == 0 {
-		return "", fmt.Errorf("пустой ответ от LLM")
-	}
-	fmt.Printf("Resp Tokens: %v", response.Usage.TotalTokens)
+// 	if resp.StatusCode != http.StatusOK {
+// 		return "", fmt.Errorf("неверный статус код: %d", resp.StatusCode)
+// 	}
 
-	return response.Choices[0].Message.Content, nil
-}
+// 	var response LocalLLMResponse
+// 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+// 		return "", fmt.Errorf("ошибка декодирования ответа: %v", err)
+// 	}
+
+// 	if len(response.Choices) == 0 {
+// 		return "", fmt.Errorf("пустой ответ от LLM")
+// 	}
+// 	fmt.Printf("Resp Tokens: %v \n", response.Usage.TotalTokens)
+
+// 	summary := response.Choices[0].Message.Content
+// 	if idx := strings.Index(summary, "--"); idx != -1 {
+// 		summary = summary[:idx]
+// 	}
+
+// 	return strings.TrimSpace(summary), nil
+// }
 
 // generateSummary создает краткую сводку с помощью локальной LLM
-func (b *Bot) generateTopic(messages string) (string, error) {
-	prompt := fmt.Sprintf(b.config.TopicPrompt, messages)
+// func (b *Bot) generateAnekdot(messages string, chatID int64) (string, error) {
+// 	// Запускаем горутину для периодической отправки индикатора печати, канал stopTyping не забываем закрыть!!!
+// 	stopTyping := make(chan struct{})
+// 	defer close(stopTyping)
+// 	go func() {
+// 		ticker := time.NewTicker(5 * time.Second) // Отправляем каждые 5 секунд
+// 		defer ticker.Stop()
+// 		for {
+// 			select {
+// 			case <-ticker.C:
+// 				if _, err := b.tgBot.Send(tgbotapi.NewChatAction(chatID, tgbotapi.ChatTyping)); err != nil {
+// 					log.Printf("Ошибка отправки индикатора печати: %v", err)
+// 				}
+// 			case <-stopTyping:
+// 				return
+// 			}
+// 		}
+// 	}()
+// 	prompt := fmt.Sprintf(b.config.AnekdotPrompt, messages)
 
-	request := LocalLLMRequest{
-		Model: b.config.AiModelName, // Имя модели может быть любым для локальной LLM
-		Messages: []LocalLLMMessage{
-			// {
-			// 	Role:    "system",
-			// 	Content: b.config.SystemPrompt,
-			// },
-			{
-				Role:    "user",
-				Content: prompt,
-			},
-		},
-		Temperature: 0.4,
-		MaxTokens:   1000,
-	}
+// 	request := LocalLLMRequest{
+// 		Model: b.config.AiModelName, // Имя модели может быть любым для локальной LLM
+// 		Messages: []LocalLLMMessage{
+// 			// {
+// 			// 	Role:    "system",
+// 			// 	Content: b.config.SystemPrompt,
+// 			// },
+// 			{
+// 				Role:    "user",
+// 				Content: prompt,
+// 			},
+// 		},
+// 		Temperature: 0.4,
+// 		MaxTokens:   1000,
+// 	}
 
-	jsonData, err := json.Marshal(request)
-	if err != nil {
-		return "", fmt.Errorf("ошибка маршалинга запроса: %v", err)
-	}
+// 	jsonData, err := json.Marshal(request)
+// 	if err != nil {
+// 		return "", fmt.Errorf("ошибка маршалинга запроса: %v", err)
+// 	}
 
-	fmt.Println("Get AI request...")
-	resp, err := b.httpClient.Post(b.config.LocalLLMUrl, "application/json", bytes.NewBuffer(jsonData))
-	if err != nil {
-		return "", fmt.Errorf("ошибка HTTP запроса: %v", err)
-	}
-	defer resp.Body.Close()
+// 	fmt.Println("Get AI request...")
+// 	resp, err := b.httpClient.Post(b.config.LocalLLMUrl, "application/json", bytes.NewBuffer(jsonData))
+// 	if err != nil {
+// 		return "", fmt.Errorf("ошибка HTTP запроса: %v", err)
+// 	}
+// 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		return "", fmt.Errorf("неверный статус код: %d", resp.StatusCode)
-	}
+// 	if resp.StatusCode != http.StatusOK {
+// 		return "", fmt.Errorf("неверный статус код: %d", resp.StatusCode)
+// 	}
 
-	var response LocalLLMResponse
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return "", fmt.Errorf("ошибка декодирования ответа: %v", err)
-	}
+// 	var response LocalLLMResponse
+// 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+// 		return "", fmt.Errorf("ошибка декодирования ответа: %v", err)
+// 	}
 
-	if len(response.Choices) == 0 {
-		return "", fmt.Errorf("пустой ответ от LLM")
-	}
-	fmt.Printf("Resp Tokens: %v", response.Usage.TotalTokens)
+// 	if len(response.Choices) == 0 {
+// 		return "", fmt.Errorf("пустой ответ от LLM")
+// 	}
+// 	fmt.Printf("Resp Tokens: %v", response.Usage.TotalTokens)
 
-	return response.Choices[0].Message.Content, nil
-}
+// 	return response.Choices[0].Message.Content, nil
+// }
+
+// generateSummary создает краткую сводку с помощью локальной LLM
+// func (b *Bot) generateTopic(messages string, chatID int64) (string, error) {
+// 	prompt := fmt.Sprintf(b.config.TopicPrompt, messages)
+
+// 	request := LocalLLMRequest{
+// 		Model: b.config.AiModelName, // Имя модели может быть любым для локальной LLM
+// 		Messages: []LocalLLMMessage{
+// 			// {
+// 			// 	Role:    "system",
+// 			// 	Content: b.config.SystemPrompt,
+// 			// },
+// 			{
+// 				Role:    "user",
+// 				Content: prompt,
+// 			},
+// 		},
+// 		Temperature: 0.4,
+// 		MaxTokens:   1000,
+// 	}
+
+// 	jsonData, err := json.Marshal(request)
+// 	if err != nil {
+// 		return "", fmt.Errorf("ошибка маршалинга запроса: %v", err)
+// 	}
+
+// 	fmt.Println("Get AI request...")
+// 	resp, err := b.httpClient.Post(b.config.LocalLLMUrl, "application/json", bytes.NewBuffer(jsonData))
+// 	if err != nil {
+// 		return "", fmt.Errorf("ошибка HTTP запроса: %v", err)
+// 	}
+// 	defer resp.Body.Close()
+
+// 	if resp.StatusCode != http.StatusOK {
+// 		return "", fmt.Errorf("неверный статус код: %d", resp.StatusCode)
+// 	}
+
+// 	var response LocalLLMResponse
+// 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+// 		return "", fmt.Errorf("ошибка декодирования ответа: %v", err)
+// 	}
+
+// 	if len(response.Choices) == 0 {
+// 		return "", fmt.Errorf("пустой ответ от LLM")
+// 	}
+// 	fmt.Printf("Resp Tokens: %v", response.Usage.TotalTokens)
+
+// 	return response.Choices[0].Message.Content, nil
+// }
