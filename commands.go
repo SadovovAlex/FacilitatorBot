70dc2924afc3a3c6348e09855dbfa4c6bd09404f
SadovovAlex <aasdvv@gmail.com>
2025-07-01 16:09:00 +0300

    .

--- commands.go
+++ commands.go
@@ -177,7 +177,7 @@ func (b *Bot) handleAISummary(message *tgbotapi.Message, count int) {
 	b.lastSummary[chatID] = time.Now()
 
 	// Генерируем изображение на основе сводки
-	description := fmt.Sprintf(b.config.ImagePrompt, summary)
+	description := fmt.Sprintf(b.config.ImagePrompt, b.truncateText(summary, 256))
 	photo, err := b.GenerateImage(description, chatID, false)
 	if err != nil {
 		// Если не удалось сгенерировать изображение, отправляем текст
